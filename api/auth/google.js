6a114b02fde8e1a7519bb38831b2540375bf77b5
Jantomz <zjaden28@gmail.com>
2025-04-26 13:15:28 -0700

    Adding twelve-labs.js

--- api/auth/google.js
+++ api/auth/google.js
@@ -3,8 +3,7 @@ import { google } from "googleapis";
 
 const CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.VITE_GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI =
-    "https://hacktech-qw7m6s69z-jantomzs-projects.vercel.app/api/auth/callback/google"; // TODO: Change this to your deployed URI for production
+const REDIRECT_URI = "http://localhost:3000"; // Change this to your deployed URI for production
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
@@ -25,31 +24,14 @@ export default async function handler(req, res) {
             const payload = ticket.getPayload();
             console.log("User Info:", payload);
 
-            // Store the user's tokens in your DB (refresh tokens)
-            // Here, let's use the tokens temporarily for demo purposes
+            // OPTIONAL: Here you can also start Gmail API calls
+            // - store tokens
+            // - or start fetching emails immediately
 
-            const { tokens } = await oauth2Client.getToken(credential);
-            oauth2Client.setCredentials(tokens);
-
-            // Now that the user is authenticated, let's list their Gmail messages
-            const gmail = google.gmail({ version: "v1", auth: oauth2Client });
-
-            // List the user's Gmail messages
-            const res = await gmail.users.messages.list({
-                userId: "me",
-                maxResults: 10, // Fetch up to 10 messages
-            });
-
-            const messages = res.data.messages || [];
-            res.send({ messages });
+            res.send({ status: "success", email: payload.email });
         } catch (error) {
-            console.error(
-                "Error verifying Google token or fetching messages",
-                error
-            );
-            res.status(400).send(
-                "Authentication failed or error fetching messages"
-            );
+            console.error("Error verifying Google token", error);
+            res.status(400).send("Authentication failed");
         }
     }
 }
